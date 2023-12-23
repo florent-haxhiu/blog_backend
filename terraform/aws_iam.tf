1ede931b24d8086ef29fd03c035d805f57d78555
Florent Haxhiu <florenthaxhiu03@gmail.com>
2023-12-23 08:24:08 +0000

    fix: Passed down account id of caller instead of using wildcard

--- terraform/aws_iam.tf
+++ terraform/aws_iam.tf
@@ -22,7 +22,7 @@ resource "aws_lambda_permission" "blog_api_gateway_method" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.blog_handler.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.aws_region}:*:${aws_api_gateway_rest_api.blog_api.id}/*/*"
+  source_arn    = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.blog_api.id}/*/*"
 }
 
 /*
--- terraform/aws_vars.tf
+++ terraform/aws_vars.tf
@@ -3,3 +3,17 @@ variable "aws_region" {
   description = "AWS Region to deploy example API Gateway REST API"
   type        = string
 }
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
