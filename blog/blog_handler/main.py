f9631b1b389c0a215804677d5601a47f733ebc4a
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-04-23 21:43:40 +0000

    add: started on the dynamodb handler

--- blog/blog_handler/main.py
+++ blog/blog_handler/main.py
@@ -1,9 +1,11 @@
 from typing import Dict, Union
 import json
+import boto3
 import aws_lambda_powertools
 import aws_lambda_powertools.utilities.typing as aws_types
 
-from blog_handler.models import BlogModel
+from core.dynamodb_handler import DynamoDbHandler
+from core.models import BlogModel
 
 logger = aws_lambda_powertools.Logger()
 
@@ -11,6 +13,11 @@ logger = aws_lambda_powertools.Logger()
 def lambda_handler(context: aws_types.LambdaContext, event: Dict[str, str]) -> Dict[str, Union[str, int]]:
     logger.info('Received ', event['body'])
     body: BlogModel = json.loads(event['body'])
+
+    dynamodb_client = boto3.client('dynamodb')
+
+    handler = DynamoDbHandler(client=dynamodb_client)
+
     return {
         'statusCode': 200,
         'body': json.dumps({'hello': 'world'})
