394ece2ae50b277c4db85d0245ff09561d53e0ea
florent-haxhiu <florenthaxhiu03@gmail.com>
2024-04-23 22:43:33 +0100

    add: function to test

--- blog/blog_handler/core/dynamodb_handler.py
+++ blog/blog_handler/core/dynamodb_handler.py
@@ -1,6 +1,7 @@
-
-
 class DynamoDbHandler:
 
-    def __init__(self, client: DynamoDBClient) -> None:
-        self._dynamodb = client
+    def __init__(self, table) -> None:
+        self._table = table
+
+    def get_item_count(self):
+        return self._table.item_count
