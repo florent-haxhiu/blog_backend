de992fac1bcc42a072357609d3d4ef706177ba7d
florent-haxhiu <florenthaxhiu03@gmail.com>
2024-04-23 22:44:50 +0100

    testing if table count shows & will test on the saving of the blog

--- blog/tests/core/test_dynamodb_handler.py
+++ blog/tests/core/test_dynamodb_handler.py
@@ -1,13 +1,44 @@
 import unittest
-from unittest.mock import Mock
+
+import boto3
+import moto
 
 from core.dynamodb_handler import DynamoDbHandler
 
 
+@moto.mock_dynamodb
 class TestDynamoDbHandler(unittest.TestCase):
 
     def setUp(self) -> None:
-        self._mock_dynamodb = Mock()
-        self._target = DynamoDbHandler(
-            client=self._mock_dynamodb
+        self._mock_dynamodb = boto3.resource('dynamodb')
+        self._mock_dynamodb.create_table(
+            TableName='blogs',
+            KeySchema=[
+                {
+                    'AttributeName': 'blogId',
+                    'KeyType': 'HASH'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'blogId',
+                    'AttributeType': 'S'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
+            }
         )
+        self._target = DynamoDbHandler(
+            table=self._mock_dynamodb.Table('blogs')
+        )
+
+    def test_table_count(self):
+        expected = 0
+
+        self.assertEqual(expected, self._mock_dynamodb.Table('blogs').item_count)
+
+
+    def test_saves_blog_to_db_with_incremental_ids(self):
+        pass
