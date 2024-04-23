2c5a90b17f978439262b5cb28498f153f7cf9dba
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-04-23 21:38:37 +0000

    change: check if filename exists

--- scripts/create_lambda_package.sh
+++ scripts/create_lambda_package.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+filename="artifact.zip"
+
+if [ -e "$filename" ]; then
+    rm artifact.zip
+    echo "File '$filename' found and now deleted"
+else
+    printf "File %s not found\nNow creating new zip", $filename
+fi
+
 poetry build
 poetry run pip install --upgrade -t package dist/*.whl
 cd package || exit
